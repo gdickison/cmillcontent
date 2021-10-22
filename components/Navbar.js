03feb4d0d2ab95d5a5c63f5b92e5f85e24eb069a
paunchymonk <gregory@dickison.net>
2021-10-22 11:18:25 -0700

    Color nav bar text red on hover and on active page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,21 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router';
 
 const NavBar = () => {
+    const router = useRouter();
+
     return (
         <div className="container">
             <nav className="navbar navbar-expand-sm navbar-light">
                 <div className="container-fluid">
                     <Link href="/portfolio" passHref>
-                        <a className="navbar-brand" href="#">Portfolio</a>
+                        <a className={router.pathname == "/portfolio" ? "navbar-brand active" : "navbar-brand"} href="#">Portfolio</a>
                     </Link>
                 </div>
                 <div className="container-fluid">
                     <Link href="/creative" passHref>
-                        <a className="navbar-brand" href="#">Creative</a>
+                        <a className={router.pathname == "/creative" ? "navbar-brand active" : "navbar-brand"} href="#">Creative</a>
                     </Link>
                 </div>
                 <div className="container-fluid">
@@ -23,13 +26,13 @@ const NavBar = () => {
                     </Link>
                 </div>
                 <div className="container-fluid">
-                    <Link href="crocodiles" passHref>
-                        <a className="navbar-brand" href="#">Crocodiles</a>
+                    <Link href="/crocodiles" passHref>
+                        <a className={router.pathname == "/crocodiles" ? "navbar-brand active" : "navbar-brand"} href="#">Crocodiles</a>
                     </Link>
                 </div>
                 <div className="container-fluid">
-                    <Link href="bio" passHref>
-                        <a className="navbar-brand" href="#">Bio</a>
+                    <Link href="/bio" passHref>
+                        <a className={router.pathname == "/bio" ? "navbar-brand active" : "navbar-brand"} href="#">Bio</a>
                     </Link>
                 </div>
             </nav>
