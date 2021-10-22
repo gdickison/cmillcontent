b50d079e40ada2bc21a5c70fda19f0294dcff3a5
paunchymonk <gregory@dickison.net>
2021-10-22 11:48:05 -0700

    Refactor active link to use id instead of class, turn home logo gray on hover

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,29 +10,29 @@ const NavBar = () => {
             <nav className="navbar navbar-expand-sm navbar-light">
                 <div className="container-fluid">
                     <Link href="/portfolio" passHref>
-                        <a className={router.pathname == "/portfolio" ? "navbar-brand active" : "navbar-brand"} href="#">Portfolio</a>
+                        <a id={router.pathname == "/portfolio" ? "active-link" : ""} className="navbar-brand" href="#">Portfolio</a>
                     </Link>
                 </div>
                 <div className="container-fluid">
                     <Link href="/creative" passHref>
-                        <a className={router.pathname == "/creative" ? "navbar-brand active" : "navbar-brand"} href="#">Creative</a>
+                        <a id={router.pathname == "/creative" ? "active-link" : ""} className="navbar-brand" href="#">Creative</a>
                     </Link>
                 </div>
                 <div className="container-fluid">
                     <Link href="/" passHref>
                         <a className="navbar-brand" href="#">
-                            <Image src="/images/logo_red.png" alt="home" width={50} height={50} />
+                            <Image id="home-link" src="/images/logo_red.png" alt="home" width={50} height={50} />
                         </a>
                     </Link>
                 </div>
                 <div className="container-fluid">
                     <Link href="/crocodiles" passHref>
-                        <a className={router.pathname == "/crocodiles" ? "navbar-brand active" : "navbar-brand"} href="#">Crocodiles</a>
+                        <a id={router.pathname == "/crocodiles" ? "active-link" : ""} className="navbar-brand" href="#">Crocodiles</a>
                     </Link>
                 </div>
                 <div className="container-fluid">
                     <Link href="/bio" passHref>
-                        <a className={router.pathname == "/bio" ? "navbar-brand active" : "navbar-brand"} href="#">Bio</a>
+                        <a id={router.pathname == "/bio" ? "active-link" : ""} className="navbar-brand" href="#">Bio</a>
                     </Link>
                 </div>
             </nav>
