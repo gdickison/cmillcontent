a17ca564d38e514a7e3ce29ffeb1f5f4d8ff2bf5
paunchymonk <gregory@dickison.net>
2021-10-25 15:08:31 -0700

    Clean up some spacing

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 			<div>
 				<Image src="/images/logo_light.png" alt="Curtis Miller Logo" width={50} height={50} />
 			</div>
-			<div>C.M. Miller</div>
+			<div className={styles.footerName}>C.M. Miller</div>
 			<div className={styles.footerText}>
 				<p>Content writing for advertising, entertainment, and publishing.</p>
 			</div>
