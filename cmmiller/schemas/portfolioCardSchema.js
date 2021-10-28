c246f6674837fd239ed7835f8cda0f9022fe2789
paunchymonk <gregory@dickison.net>
2021-10-28 16:14:55 -0700

    Set up sanity io for the portfolio writing samples sections

---
+++ cmmiller/schemas/portfolioCardSchema.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    title: "Portfolio Card",
+    name: "portfolioCard",
+    type: "document",
+    fields: [
+        {
+            title: "Date",
+            name: "date",
+            type: "string",
+            validation: Rule => Rule.required()
+        },
+        {
+            title: "Title",
+            name: "title",
+            type: "string",
+            validation: Rule => Rule.required()
+        },
+        {
+            title: "Link",
+            name: "link",
+            type: "url",
+            validation: Rule => Rule.required()
+        },
+        {
+            title: "Image",
+            name: "image",
+            type: "image",
+            validation: Rule => Rule.required()
+        }
+    ]
+}
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from "@sanity/client";
+
+export default sanityClient({
+    projectId: "v0cf8465",
+    dataset: "production"
+})
