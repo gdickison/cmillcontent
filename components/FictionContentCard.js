b7ba5f2fcc39b2de13ec7f136b3fdaf4f8bb70f3
paunchymonk <gregory@dickison.net>
2021-11-23 17:17:59 -0800

    Make fiction and literary section responsive

--- components/FictionContentCard.js
+++ components/FictionContentCard.js
@@ -2,11 +2,11 @@ const FictionContentCard = ({title, genre, description, link}) => {
     return (
         <div className="creative-fictionContentCard">
             <div className="creative-fictionContentCardGrid">
-                <div className="creative-fictionContentCardContents">
+                <div className="creative-fictionContentCardContents" id="fl-title">
                     <p className="creative-fictionTitle"><a href={link} target="_blank" rel="noreferrer">{title}</a></p>
                     <p className="creative-fictionGenre">{genre}</p>
                 </div>
-                <div className="creative-fictionContentCardContents">
+                <div className="creative-fictionContentCardContents" id="fl-description">
                     <p className="creative-fictionDescription">{description}</p>
                 </div>
             </div>
