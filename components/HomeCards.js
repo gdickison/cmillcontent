a8fe955733b757b42ebcaefb482f37bad52c52d6
paunchymonk <gregory@dickison.net>
2021-11-30 16:52:41 -0800

    Connect HomeCards to Sanity

--- components/HomeCards.js
+++ components/HomeCards.js
@@ -1,81 +1,86 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Loader from "../components/Loader"
+import { useState, useEffect, Fragment } from 'react'
+import sanityClient from '../src/client'
+import PortableText from '@sanity/block-content-to-react'
 
 const HomeCards = () => {
+    const [homecardData, setHomecardData] = useState(null);
+
+    useEffect(() => {
+        sanityClient.fetch(`*[_type == "homecards"] {
+            _id,
+            hc_1_header,
+            hc_1_text,
+            hc_2_header,
+            hc_2_text,
+            hc_3_header,
+            hc_3_text
+        }`)
+        .then((data) => setHomecardData(data));
+    }, []);
+
     return (
         <div className="homeCards-cardContainer">
-            <div className="homeCards-card">
-                <div className="homeCards-cardIcon">
-                    <Image src="/images/icon_megaphone.png" alt="megaphone" width={80} height={80} />
-                </div>
-                <h2 className="homeCards-cardHeader">Marketing and Advertising</h2>
-                <div className="homeCards-cardContentList">
-                    <ul>
-                        <li>
-                            Content writing and editing, ghostwriting, blogging, script writing, ad copy, site copy, social media
-                        </li>
-                        <li>
-                            Revising, editing, brainstorming, outlining, proofreading
-                        </li>
-                    </ul>
-                </div>
-                <div className="homeCards-cardLinkContainer">
-                    <Link href="/portfolio" passHref>
-                        <span className="homeCards-cardLink">Visit Portfolio</span>
-                    </Link>
-                </div>
-            </div>
-            <div className="homeCards-card">
-                <div className="homeCards-cardIcon">
-                    <Image src="/images/icon_monitor.png" alt="megaphone" width={80} height={80} />
-                </div>
-                <h2 className="homeCards-cardHeader">Entertainment</h2>
-                <div className="homeCards-cardContentList">
-                    <ul>
-                        <li>
-                            Script and content writing for film, theater, television, web, and podcast
-                        </li>
-                        <li>
-                            Revising, polishing, editing, and proofreading
-                        </li>
-                        <li>
-                            Story development
-                        </li>
-                        <li>
-                            Submission reading and feedback
-                        </li>
-                    </ul>
-                </div>
-                <div className="homeCards-cardLinkContainer">
-                    <Link href="/creative" passHref>
-                        <span className="homeCards-cardLink">Visit Creative</span>
-                    </Link>
-                </div>
-            </div>
-            <div className="homeCards-card">
-                <div className="homeCards-cardIcon">
-                    <Image src="/images/icon_book.png" alt="megaphone" width={80} height={80} />
-                </div>
-                <h2 className="homeCards-cardHeader">Publishing and Literary</h2>
-                <div className="homeCards-cardContentList">
-                    <ul>
-                        <li>
-                            Writing and ghostwriting (fiction, memoir, creative nonfiction), story development
-                        </li>
-                        <li>
-                            Submission reading and proofreading
-                        </li>
-                        <li>
-                            Copy and line editing
-                        </li>
-                    </ul>
-                </div>
-                <div className="homeCards-cardLinkContainer">
-                    <Link href="/crocodiles" passHref>
-                        <span className="homeCards-cardLink">Visit Blog</span>
-                    </Link>
-                </div>
-            </div>
+        {!homecardData
+            ?
+            <Loader />
+            :
+            homecardData.map((data) => {
+                return (
+                    <Fragment key={data._id}>
+                        <div className="homeCards-card">
+                            <div className="homeCards-cardIcon">
+                                <Image src="/images/icon_megaphone.png" alt="megaphone" width={80} height={80} />
+                            </div>
+                            <h2 className="homeCards-cardHeader">{data.hc_1_header}</h2>
+                            <div className="homeCards-cardContentList">
+                                <PortableText
+                                    blocks={data.hc_1_text}
+                                />
+                            </div>
+                            <div className="homeCards-cardLinkContainer">
+                                <Link href="/portfolio" passHref>
+                                    <span className="homeCards-cardLink">Visit Portfolio</span>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="homeCards-card">
+                            <div className="homeCards-cardIcon">
+                                <Image src="/images/icon_monitor.png" alt="megaphone" width={80} height={80} />
+                            </div>
+                            <h2 className="homeCards-cardHeader">{data.hc_2_header}</h2>
+                            <div className="homeCards-cardContentList">
+                                <PortableText
+                                    blocks={data.hc_2_text}
+                                />
+                            </div>
+                            <div className="homeCards-cardLinkContainer">
+                                <Link href="/creative" passHref>
+                                    <span className="homeCards-cardLink">Visit Creative</span>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="homeCards-card">
+                            <div className="homeCards-cardIcon">
+                                <Image src="/images/icon_book.png" alt="megaphone" width={80} height={80} />
+                            </div>
+                            <h2 className="homeCards-cardHeader">{data.hc_3_header}</h2>
+                            <div className="homeCards-cardContentList">
+                                <PortableText
+                                    blocks={data.hc_3_text}
+                                />
+                            </div>
+                            <div className="homeCards-cardLinkContainer">
+                                <Link href="/crocodiles" passHref>
+                                    <span className="homeCards-cardLink">Visit Blog</span>
+                                </Link>
+                            </div>
+                        </div>
+                    </Fragment>
+                )
+            })}
         </div>
     )
 }
