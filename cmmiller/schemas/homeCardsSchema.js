e0b4a9ee0e218b28c31752364ecbd9278421b295
paunchymonk <gregory@dickison.net>
2021-12-01 10:50:28 -0800

    Refactor homecards schema and homecards component to better use Sanity

--- cmmiller/schemas/homeCardsSchema.js
+++ cmmiller/schemas/homeCardsSchema.js
@@ -5,15 +5,21 @@ export default {
     type: "document",
     fields: [
         {
-            title: "HomeCard 1 Header",
-            name: "hc_1_header",
+            title: "Home Card Icon",
+            name: "hc_icon",
+            type: "image",
+            validation: Rule => Rule.required()
+        },
+        {
+            title: "Home Card Header",
+            name: "hc_header",
             type: "string",
             validation: Rule => Rule.required().max(25),
             description: "Limit of 25 characters"
         },
         {
-            title: "HomeCard 1 Text",
-            name: "hc_1_text",
+            title: "Home Card Content",
+            name: "hc_content",
             type: "array",
             of: [
                 {
@@ -36,66 +42,19 @@ export default {
             ]
         },
         {
-            title: "HomeCard 2 Header",
-            name: "hc_2_header",
+            title: "Button Text",
+            name: "hc_button_text",
             type: "string",
-            validation: Rule => Rule.required().max(25),
-            description: "Limit of 25 characters"
+            validation: Rule => Rule.required()
         },
         {
-            title: "HomeCard 2 Text",
-            name: "hc_2_text",
-            type: "array",
-            of: [
-                {
-                    type: "block",
-                    styles: [
-                        {title: "Normal", value: "normal"}
-                    ],
-                    marks: {
-                        decorators: [
-                            {title: "Strong", value: "strong"},
-                            {title: "Emphasis", value: "em"},
-                            {title: "Underline", value: "underline"}
-                        ],
-                        annotations: []
-                    },
-                    lists: [
-                        {title: "Bullet", value: "bullet"}
-                    ]
-                }
-            ]
-        },
-        {
-            title: "HomeCard 3 Header",
-            name: "hc_3_header",
-            type: "string",
-            validation: Rule => Rule.required().max(25),
-            description: "Limit of 25 characters"
-        },
-        {
-            title: "HomeCard 3 Text",
-            name: "hc_3_text",
-            type: "array",
-            of: [
-                {
-                    type: "block",
-                    styles: [
-                        {title: "Normal", value: "normal"}
-                    ],
-                    marks: {
-                        decorators: [
-                            {title: "Strong", value: "strong"},
-                            {title: "Emphasis", value: "em"},
-                            {title: "Underline", value: "underline"}
-                        ],
-                        annotations: []
-                    },
-                    lists: [
-                        {title: "Bullet", value: "bullet"}
-                    ]
-                }
-            ]
+            title: "Button Link",
+            name: "hc_button_link",
+            type: "url",
+            validation: Rule => Rule.required().uri({
+                allowRelative: true,
+                // scheme: [""]
+            })
         }
     ]
 }
